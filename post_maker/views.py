9b475c2990258139ff2ac744b6c4cd602b65ee50
Tawa <tmfishman13@gmail.com>
2021-07-16 13:22:33 -0400

    Added in missing fields and some dummy data. Tried to mess around with functions as well but no luck yet

--- post_maker/views.py
+++ post_maker/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
+posts = [
+    {'title': 'My First Post',
+     'body': 'just my first post, hi!',
+     'date_created': 'July 16, 2021',
+     'author': 'Mary Sue',
+     'no_of_likes': 0,
+     'no_of_comments': 0}
+]
+
 def post_creation(request):
-    return HttpResponse("<h1> Post Creation Placeholder </h1>")
+    return HttpResponse('<h1> Post Creation Page </h1>')
