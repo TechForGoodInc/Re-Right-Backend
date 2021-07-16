9b19244f0cfe0c24239fb44fcc8f2cc92340df23
Tawa <tmfishman13@gmail.com>
2021-07-16 15:24:39 -0400

    Started using serializers in Post creation

--- post_maker/views.py
+++ post_maker/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from .models import Post
+from .serializers import PostSerializer
 # Create your views here.
 
 posts = [
@@ -11,6 +20,12 @@ posts = [
      'no_of_likes': 0,
      'no_of_comments': 0}
 ]
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    http_method_names = ['get', 'put', 'delete']
+    permission_classes = [IsAuthenticated]
+
 
 def post_creation(request):
     return HttpResponse('<h1> Post Creation Page </h1>')
